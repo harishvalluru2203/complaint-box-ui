2931ab368314472ef43211056c856109a5f394d4
harishvalluru2203 <harish.valluru22@outlook.com>
2024-02-08 08:39:19 +0530

    build issue fix 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Complaint Box
-            </Typography>
-            <Button color="inherit">
-              <Link href="/users">USER</Link>
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
-  );
+  return <>APP - Home Page</>;
 }
